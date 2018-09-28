c9a8288d23d752d17f541394d3b203bdb2a0e760
jiluohao <472327024@qq.com>
2018-09-28 16:49:42 +0800

    新增表结构文件（待完善）

--- schema/schema.sql
+++ schema/schema.sql
@@ -4,7 +4,7 @@ create table user
 	userAccount varchar(128) not null,
 	passwd varchar(64) not null,
 	createTime timestamp default CURRENT_TIMESTAMP not null,
-	updateTime timestamp default CURRENT_TIMESTAMP not null,
+	updateTime timestamp default CURRENT_TIMESTAMP not null on update current_timestamp ,
 	state tinyint default '1' not null
 )
 ;

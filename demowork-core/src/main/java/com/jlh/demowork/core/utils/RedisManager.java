b367c5bf8858066c66feac74c9fea2e83f4065c4
jiluohao <472327024@qq.com>
2018-09-28 16:24:14 +0800

    redisManager

---
+++ demowork-core/src/main/java/com/jlh/demowork/core/utils/RedisManager.java
@@ -0,0 +1,10 @@
+package com.jlh.demowork.core.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jiluohao@yixin.im
+ * Date: 2018-09-28 16:21
+ * Description:
+ */
+public class RedisManager {
+}

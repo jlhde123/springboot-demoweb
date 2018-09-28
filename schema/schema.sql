1a982f6f38c0e08daaa41ba0d1a42507c8fdb424
jiluohao <472327024@qq.com>
2018-09-28 16:48:01 +0800

    readme内容更新

---
+++ schema/schema.sql
@@ -0,0 +1,11 @@
+create table user
+(
+	id bigint auto_increment primary key,
+	userAccount varchar(128) not null,
+	passwd varchar(64) not null,
+	createTime timestamp default CURRENT_TIMESTAMP not null,
+	updateTime timestamp default CURRENT_TIMESTAMP not null,
+	state tinyint default '1' not null
+)
+;
+
